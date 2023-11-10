4ecf4fb12ca03ca3e34827845d9d71343398820a
spelzi <60978130+spelzi@users.noreply.github.com>
2023-11-10 11:40:34 +0100

    update repo

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -3,12 +3,11 @@ const dotenv = require("dotenv");
 
 const MONGO_URL = MONGO_URL;
 const MONGO_URL2 = MONGO_URL2;
-  
 
 const mongoConnect = async () => {
   mongoose
     .connect(MONGO_URL)
-    .then(() => console.log("db connection is ready"))
+    .then(() => console.log("db connection is ready..."))
     .catch((error) => console.log(error));
 };
 
